04452e29449ac217635852124e6159d5909027d5
Eze Sandes <jezesandes@gmail.com>
2021-10-16 19:55:13 -0300

    Completed mobile design. Added favicon. Added web description. Added smooth scroll

--- js/main.js
+++ js/main.js
@@ -16,3 +16,30 @@ function showMenu() {
 }
 
 showMenu();
+
+/************************************************ */
+/****************** SMOOTH SCROLL ********************** */
+/************************************************ */
+
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
